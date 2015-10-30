a731bcc4e40356469dcf71cfc86e0f799614cab9
Sufijen Bani <sbani@users.noreply.github.com>
2015-10-30 14:31:44 +0000

    Added ordinal functionality

---
+++ number/ordinal/ordinal.go
@@ -0,0 +1,30 @@
+package humanizer
+
+import (
+    "math"
+    "strconv"
+)
+
+func Ordinal(number int) string {
+    absNumber := int(math.Abs(float64(number)))
+    
+    i := absNumber % 100
+    if i == 11 || i == 12 || i == 13 {
+        return "th"
+    }
+    
+    switch absNumber % 10 {
+    case 1:
+        return "st"
+    case 2:  
+        return "nd"
+    case 3:
+        return "rd"
+    default:
+        return "th"
+    }
+}
+
+func Odinalize(number int) string {
+    return strconv.Itoa(number) + Ordinal(number)
+}
