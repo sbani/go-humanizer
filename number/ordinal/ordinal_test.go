6687667d424d7f424be0c8f00c9d28f6d10c91b0
Sufijen Bani <cloud9@sbani.net>
2015-10-30 14:31:44 +0000

    Added ordinal functionality

---
+++ number/ordinal/ordinal_test.go
@@ -0,0 +1,22 @@
+package humanizer
+
+import (
+    "testing"
+    "github.com/stretchr/testify/assert"
+)
+
+func TestOrdinalizesNumbers(t *testing.T) {
+    assert.Equal(t, "1st", Odinalize(1))
+    assert.Equal(t, "2nd", Odinalize(2))
+    assert.Equal(t, "23rd", Odinalize(23))
+    assert.Equal(t, "1002nd", Odinalize(1002))
+    assert.Equal(t, "-111th", Odinalize(-111))
+}
+
+func TestReturnsOrdinalSuffix(t *testing.T) {
+    assert.Equal(t, "st", Ordinal(1))
+    assert.Equal(t, "nd", Ordinal(2))
+    assert.Equal(t, "rd", Ordinal(23))
+    assert.Equal(t, "nd", Ordinal(1002))
+    assert.Equal(t, "th", Ordinal(-111))
+}
