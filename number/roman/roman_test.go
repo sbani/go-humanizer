770960a5e8807b1f5bdbf2df58c308e47b042d0a
Sufijen Bani <cloud9@sbani.net>
2015-10-30 15:56:21 +0000

    Added roman functionality

---
+++ number/roman/roman_test.go
@@ -0,0 +1,59 @@
+package humanizer
+
+import (
+    "testing"
+    "github.com/stretchr/testify/assert"
+)
+
+var workingTests = map[string]int{
+
+    "CXXV": 125,
+}
+
+func TestToRoman(t *testing.T) {
+    for expected, input := range workingTests {
+        output, err := ToRoman(input)
+
+        if err == nil {
+            assert.Equal(t, expected, output)
+        } else {
+            assert.Fail(t, "An error was given unexpectatly")
+        }
+    }
+}
+
+func TestToRomanError(t *testing.T) {
+    tests := []int{
+        -1,
+        4000,
+    }
+    
+    for _, input := range tests {
+        _, err := ToRoman(input)
+        assert.EqualError(t, err, "Number not convertable")
+    }
+}
+
+func TestFromRoman(t *testing.T) {
+    for input, expected := range workingTests {
+        output, err := FromRoman(input)
+        
+        if err == nil {
+            assert.Equal(t, expected, output)
+        } else {
+            assert.Fail(t, "An error was given unexpectatly")
+        }
+    }
+}
+
+func TestFromRomanError(t *testing.T) {
+    tests := []string{
+        "foobar",
+        "",
+    }
+    
+    for _, input := range tests {
+        _, err := FromRoman(input)
+        assert.EqualError(t, err, "String not convertable")
+    }
+}
