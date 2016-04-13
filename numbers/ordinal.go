9ef34ecda09082907d71489d78aa535680375565
Sufijen Bani <sbani@users.noreply.github.com>
2016-04-13 08:23:58 +0200

    Fixed bugs and fixed code with golint and gofmt (#1)
    - Checked code with gofmt
    - Checkd code with golint
    - Renamed func because of a missing letter
    - Added go report card

--- numbers/ordinal.go
+++ numbers/ordinal.go
@@ -1,32 +1,32 @@
 package numbers
 
 import (
-    "math"
-    "strconv"
+	"math"
+	"strconv"
 )
 
-// Get the ordinal only for the specific number.
+// Ordinal returns the ordinal string for a specific integer.
 func Ordinal(number int) string {
-    absNumber := int(math.Abs(float64(number)))
-    
-    i := absNumber % 100
-    if i == 11 || i == 12 || i == 13 {
-        return "th"
-    }
-    
-    switch absNumber % 10 {
-    case 1:
-        return "st"
-    case 2:  
-        return "nd"
-    case 3:
-        return "rd"
-    default:
-        return "th"
-    }
+	absNumber := int(math.Abs(float64(number)))
+
+	i := absNumber % 100
+	if i == 11 || i == 12 || i == 13 {
+		return "th"
+	}
+
+	switch absNumber % 10 {
+	case 1:
+		return "st"
+	case 2:
+		return "nd"
+	case 3:
+		return "rd"
+	default:
+		return "th"
+	}
 }
 
 // Ordinalize the number by adding the Ordinal to the number.
-func Odinalize(number int) string {
-    return strconv.Itoa(number) + Ordinal(number)
+func Ordinalize(number int) string {
+	return strconv.Itoa(number) + Ordinal(number)
 }
